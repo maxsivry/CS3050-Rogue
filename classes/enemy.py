84e5ab0f69fe6f15f2502d73ab3448b53f6d0aed
Evan C Satterfield <evan.satterfield@uvm.edu>
2023-11-15 00:20:05 -0500

    Combat updates + Added more monsters

--- classes/enemy.py
+++ classes/enemy.py
@@ -6,12 +6,26 @@ import sys
 import project_constants as constants
 from classes.actor import Actor
 
+CRAB_CHANCE = 30
+WRAITH_CHANCE = 10
+DRAGON_CHANCE = 1
 
-def create_monsters():
+def create_monsters(level):
     # Create area to store created objects
     monster_list = []
-    
-    monster_list.append(Enemy(filename="static/enemy.png", scale=constants.SPRITE_SCALING))
+
+    num_monsters = (int) (random.randint(2,5) + level*.8)
+
+    for _ in range(num_monsters+5):
+        monster_type = random.randint(0, 100-level)
+        if monster_type < DRAGON_CHANCE and level > 3:
+            monster_list.append(Dragon(filename="static/dragon.png", scale=constants.SPRITE_SCALING))
+        if monster_type < WRAITH_CHANCE:
+            monster_list.append(Wraith(filename="static/wraith.png", scale=constants.SPRITE_SCALING))
+        elif monster_type < CRAB_CHANCE:
+            monster_list.append(Crab(filename="static/crab.png", scale=constants.SPRITE_SCALING))
+        else:
+            monster_list.append(Slime(filename="static/slime.png", scale=constants.SPRITE_SCALING))
 
     return monster_list
 
@@ -26,11 +40,18 @@ class Enemy(Actor):
         self.health = 7
         self.name = "Slime"
         self.is_alive = True
+        # XP rewarded upon defeat
+        self.reward = 1
 
-    def attack(self, player):
+    def get_damage(self):
         hit = random.randint(1, 10)
         if hit > 5:
-            damage = random.randint(1, 3)
+            return random.randint(1, 3)
+        return 0
+
+    def attack(self, player):
+        damage = self.get_damage()
+        if damage > 0:
             true_damage = damage - player.get_defense()
             if true_damage > 0:
                 if player.take_damage(true_damage):
@@ -69,3 +90,80 @@ class Enemy(Actor):
     # Default monster doesn't do anything
     def chase(self, player, game):
         pass
+
+
+# Basic
+class Slime(Enemy):
+    def __init__(
+            self,
+            filename: str = None,
+            scale: float = 1
+    ):
+        super().__init__(filename=filename, scale=scale)
+        self.health = 7
+        self.name = "Slime"
+        self.is_alive = True
+        self.reward = random.randint(1,2)
+    
+    def get_damage(self):
+        hit = random.randint(1, 10)
+        if hit > 5:
+            return random.randint(1, 3)
+        return 0
+
+# Weak but has a lot of health
+class Crab(Enemy):
+    def __init__(
+            self,
+            filename: str = None,
+            scale: float = 1
+    ):
+        super().__init__(filename=filename, scale=scale)
+        self.health = random.randint(10,14)
+        self.name = "Crab"
+        self.is_alive = True
+        self.reward = random.randint(2,3)
+    
+    def get_damage(self):
+        hit = random.randint(1, 10)
+        if hit > 7:
+            return random.randint(1, 2)
+        return 0
+
+# Glass Cannon
+class Wraith(Enemy):
+    def __init__(
+            self,
+            filename: str = None,
+            scale: float = 1
+    ):
+        super().__init__(filename=filename, scale=scale)
+        self.health = random.randint(1,4)
+        self.name = "Wraith"
+        self.is_alive = True
+        self.reward = random.randint(3,4)
+    
+    def get_damage(self):
+        hit = random.randint(1, 10)
+        if hit > 4:
+            return random.randint(1, 2) + random.randint(1, 2)
+        return 0
+
+# Very rare; You're screwed if you try to fight this at low level
+class Dragon(Enemy):
+    def __init__(
+            self,
+            filename: str = None,
+            scale: float = 1
+    ):
+        super().__init__(filename=filename, scale=scale)
+        self.health = random.randint(15,20)
+        self.name = "Dragon"
+        self.is_alive = True
+        self.reward = 20
+    
+    def get_damage(self):
+        hit = random.randint(1, 10)
+        if hit > 2:
+            return random.randint(1, 3) + 5
+        return 0
