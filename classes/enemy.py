aa44b4944a348e1808a90355094af0b0ca4ba208
Max S Ivry <max.ivry@uvm.edu>
2023-11-15 20:29:18 -0500

    Update enemy.py

--- classes/enemy.py
+++ classes/enemy.py
@@ -82,74 +82,66 @@ class Enemy(Actor):
         if self.is_near(player, game):
             self.attack(player)
         else:
-            self.chase(player, game)
+            self.chase(player, game, direction=0)
 
     # The monsters movement turn
     # Takes in the player and the games state
     # to calculate where the monster should move
     # Default monster doesn't do anything
     def chase(self, player, grid, direction):
+        pass
         
-        #returns true if move is valid
-        def checkvalid(grid, rM, cM):
-            return ((grid[rM, cM].tile_type != TileType.Wall) and (rM < constants.ROW_COUNT) and (cM < constants.COLUMN_COUNT) and (rM >= 0) and
-                (cM >= 0))
+        # #returns true if move is valid
+        # def checkvalid(grid, rM, cM):
+        #     return ((grid[rM, cM].tile_type != TileType.Wall) and (rM < constants.ROW_COUNT) and (cM < constants.COLUMN_COUNT) and (rM >= 0) and
+        #         (cM >= 0))
 
-        #convert location to tile location player
-        playercol = int(player.center_x / constants.TILE_WIDTH)
-        playerrow = int(player.center_y / constants.TILE_HEIGHT)
-        if direction == 'Up':
-            playerrow += 1
-        if direction == 'Down':
-            playerrow -= 1
-        if direction == 'Right':
-            playercol += 1
-        if direction == 'Left':
-            playercol -= 1
+        # #convert location to tile location player
+        # playercol = int(player.center_x / constants.TILE_WIDTH)
+        # playerrow = int(player.center_y / constants.TILE_HEIGHT)
+        # if direction == 'Up':
+        #     playerrow += 1
+        # if direction == 'Down':
+        #     playerrow -= 1
+        # if direction == 'Right':
+        #     playercol += 1
+        # if direction == 'Left':
+        #     playercol -= 1
 
-        #convert location to tile location monster
-        monstercol = int(self.center_x / constants.TILE_WIDTH)
-        monsterrow = int(self.center_y / constants.TILE_HEIGHT)
-        newmonstercol = monstercol
-        newmonsterrow = monsterrow
+        # #convert location to tile location monster
+        # monstercol = int(self.center_x / constants.TILE_WIDTH)
+        # monsterrow = int(self.center_y / constants.TILE_HEIGHT)
+        # newmonstercol = monstercol
+        # newmonsterrow = monsterrow
 
-        #if monster is close enough to "see" (if monster row or col is within 10)
-        if (abs(monstercol - playercol) < 10) and (abs(monsterrow - playerrow) < 10):
+        # #if monster is close enough to "see" (if monster row or col is within 10)
+        # if (abs(monstercol - playercol) < 10) and (abs(monsterrow - playerrow) < 10):
 
-            #try first moving a row closer to player (y)
-            temp_row = -1
-            to_change = constants.TILE_HEIGHT
-            if monsterrow < playerrow:
-                temp_row = monsterrow +1
-            elif monsterrow > playerrow:
-                temp_row = monsterrow -1 
-                to_change = - (to_change)
-            #if move legal make move
-            if checkvalid(grid, temp_row, monstercol):
-                self.change_y += to_change
-                self.change_x = 0
+        #     #try first moving a row closer to player (y)
+        #     temp_row = -1
+        #     to_change = constants.TILE_HEIGHT
+        #     if monsterrow < playerrow:
+        #         temp_row = monsterrow +1
+        #     elif monsterrow > playerrow:
+        #         temp_row = monsterrow -1 
+        #         to_change = - (to_change)
+        #     #if move legal make move
+        #     if checkvalid(grid, temp_row, monstercol):
+        #         self.change_y += to_change
+        #         self.change_x = 0
 
-            #else try to move column closer to player (x)
-            else:
-                temp_col = -1
-                to_change = constants.TILE_WIDTH
-                if monstercol > playercol:
-                    temp_col = monstercol -1
-                    to_change = - (to_change)
-                elif monstercol < playercol:
-                    temp_col = monstercol +1
-                if checkvalid(grid, monsterrow, temp_col):
-                    self.change_x += to_change
-                    self.change_y = 0
+        #     #else try to move column closer to player (x)
+        #     else:
+        #         
 
-            #if monster does not move, it means that monster is either overlapping with players predicted move or no legal moves for monster
+        #     #if monster does not move, it means that monster is either overlapping with players predicted move or no legal moves for monster
 
         
-        #else
-            #make arbitrary move
+        # #else
+        #     #make arbitrary move
         
-        # if row and col overlap player row and player col
-            #battle!
+        # # if row and col overlap player row and player col
+        #     #battle!
 # Basic
 class Slime(Enemy):
     def __init__(
